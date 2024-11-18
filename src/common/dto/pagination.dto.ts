eef63f26e20959113892bdd5bc22fd898dfde407
Jesús Mira <jesmira@hotmail.es>
2024-11-18 10:44:42 +0100

    Init proyect, tables relations and init files

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,14 @@
+import { IsOptional, IsPositive, Min } from "class-validator";
+import { Type } from 'class-transformer';
+
+export class PaginationDto{
+    @IsOptional()
+    @IsPositive()
+    @Type(() => Number) // enableImplicitConversions:true
+    limit?: number;
+    
+    @IsOptional()
+    @Min(0)
+    @Type(() => Number) // enableImplicitConversions:true
+    offset?: number;
+}
